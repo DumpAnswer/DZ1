8d511152cecbb8d5d2f997f1ce827d99f5e6f24f
Alexey Kuznetsov <kaleksers93@gmail.com>
2021-01-16 12:30:50 +0300

    DomZadanie1

---
+++ Dz1/Dz1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomZad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, как тебя зовут ? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет,{name}!Сегодня {DateTime.Today}");
+            Console.ReadKey();
+        }
+    }
+}
+ 
