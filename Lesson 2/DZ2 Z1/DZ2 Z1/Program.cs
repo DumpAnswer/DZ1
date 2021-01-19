8ba4aedd58ab7edfa5d3b6dd91b2d60b99945e2d
Alexey Kuznetsov <kaleksers93@gmail.com>
2021-01-19 14:11:06 +0300

    The first task

---
+++ Lesson 2/DZ2 Z1/DZ2 Z1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ2_Z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какая самая высокая темепратура была сегодня ?");
+            string MaxTemp = Console.ReadLine();
+            int a = Convert.ToInt32(MaxTemp);
+            Console.WriteLine("А какая самая низкая за сегодня? ");
+            string MinTemp = Console.ReadLine();
+            int b =  Convert.ToInt32(MinTemp);
+            int MedTemp = (a + b) / 2;
+            Console.WriteLine($"Тогда средняя температура за сегодня {MedTemp} градусов !");
+            Console.ReadKey();
+        }
+    }
+}
