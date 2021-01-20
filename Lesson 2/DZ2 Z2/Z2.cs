52ad7b7a4b7e01e7eea69c44e27a6469f8769879
Alexey Kuznetsov <kaleksers93@gmail.com>
2021-01-20 14:36:32 +0300

    Complete task 2

---
+++ Lesson 2/DZ2 Z2/Z2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ2_Z2
+{
+    class Z2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите порядковый номер текушего месяца:");
+            string Month = Console.ReadLine();
+            int a = Convert.ToInt32(Month);
+            string NameMonth = "";
+            switch (a)
+            {
+                case 1:
+                    NameMonth = "Январь";
+                    break;
+                case 2:
+                    NameMonth = "Февраль";
+                    break;
+                case 3:
+                    NameMonth = "Март";
+                    break;
+                case 4:
+                    NameMonth = "Апрель";
+                    break;
+                case 5:
+                    NameMonth = "Май";
+                    break;
+                case 6:
+                    NameMonth = "Июнь";
+                    break;
+                case 7:
+                    NameMonth = "Июль";
+                    break;
+                case 8:
+                    NameMonth = "Август";
+                    break;
+                case 9:
+                    NameMonth = "Сентябрь";
+                    break;
+                case 10:
+                    NameMonth = "Ноябрь";
+                    break;
+                case 11:
+                    NameMonth = "Декабрь";
+                    break;
+                case 12:
+                    NameMonth = "Воскресенье";
+                    break;
+            }
+
+            Console.WriteLine(NameMonth);
+            Console.ReadKey();
+        }
+    }
+}
