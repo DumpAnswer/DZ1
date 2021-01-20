8fa5f90249988670c46b220b47a4ac6cf000bc03
Alexey Kuznetsov <kaleksers93@gmail.com>
2021-01-20 16:37:43 +0300

    4 задание

---
+++ Lesson 2/DZ2 Z4/Z4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ2_Z4
+{
+    class Z4
+    {
+        static void Main(string[] args)
+        {
+            string Firm = "AO 'ФПК'";
+            Console.WriteLine(Firm);
+            
+            string IP = "ИП Васильев.О.О";
+            Console.WriteLine(IP);
+
+            string KKT = "ККТ 0022352565250             #5356";
+            Console.WriteLine(KKT);
+
+            string INN = "Инн 4556287625                10.02.19 15:44";
+            Console.WriteLine(INN);
+
+            string name = "Ангальд";
+            Console.WriteLine(name);
+
+            string goWork = "Кассовый чек/Приход";
+            Console.WriteLine(goWork);
+
+            string website = "Сайт ФНС:                     www.nalog.ru";
+            Console.WriteLine(website);
+
+            string product = "Ж/д билет Москва - Сочи";
+            Console.WriteLine(product);
+
+            string price = "1                             =10000.00";
+            Console.WriteLine(price);
+
+            string all = "Всего к оплате                =10000.00";
+            Console.WriteLine(all);
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
