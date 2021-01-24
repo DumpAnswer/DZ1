fbdb3ac4b1d66744bf3b5462482f712cbac97518
Alexey Kuznetsov <kaleksers93@gmail.com>
2021-01-25 00:06:11 +0300

    Задание №3 выполнено

---
+++ Lesson 3/DZ3_Z3/DZ3 Z3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace DZ3_Z3
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+
+
+
+		{
+
+			string say = "Hello";
+
+			for (int i = say.Length - 1; i >= 0; i--)
+			{
+				Console.Write(say[i]);
+				Console.ReadKey();
+
+
+			}
+		}
+	}
+}
+
+
