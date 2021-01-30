6c01254ab5414412f343947673a7858a36063fe0
Alexey Kuznetsov <kaleksers93@gmail.com>
2021-01-31 00:24:40 +0300

    Выполнил 2 задание

---
+++ Lesson 4/DZ4 Z2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ4_Z2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа через пробел");
+            string[] words = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int sum = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                int n = int.Parse(words[i]);
+                sum += n;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
