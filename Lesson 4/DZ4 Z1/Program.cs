30cbdcb0e37c14cc4439fc7a12d68f5bfd764515
Alexey Kuznetsov <kaleksers93@gmail.com>
2021-01-30 21:35:18 +0300

    Выполнил первое задание

---
+++ Lesson 4/DZ4 Z1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ4_Z1
+{
+    class Program
+    {
+
+        static void GetFullName(string firstName, string lastName, string patronymic)
+        {
+            Console.WriteLine($"{firstName} {lastName} {patronymic}");
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            GetFullName("Алекс", "ДеАрман", "Аластаравич");
+            GetFullName("Рионар", "Риндур", "Рикардович");
+            GetFullName("Салем", "Темирен", "Тибальдович");
+            GetFullName("Ирина", "Родина", "Михайловна");
+            Console.ReadKey();
+
+        }
+
+    }
+}   
+
+
