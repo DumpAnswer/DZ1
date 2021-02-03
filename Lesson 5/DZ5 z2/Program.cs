b494f04dcd4178024fdc9ae7225f02ed430d83c7
Alexey Kuznetsov <kaleksers93@gmail.com>
2021-02-03 15:28:36 +0300

    Выполнил задание 2

---
+++ Lesson 5/DZ5 z2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DZ5_z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename, time;
+            filename = "startup.txt";
+            time = Convert.ToString(DateTime.Now);
+            File.WriteAllText(filename, time);
+        }
+    }
+}
