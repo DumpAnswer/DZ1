48570e941b011ae64ecf32171304b6fef2c10366
Alexey Kuznetsov <kaleksers93@gmail.com>
2021-02-03 15:14:57 +0300

    Выполнил задание 1

---
+++ Lesson 5/DZ5 Z1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DZ5_Z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "Dz1.txt";
+            Console.WriteLine("Введите любой текст !");
+            string afile = Console.ReadLine();
+            File.WriteAllText(filename, afile);
+
+        }
+    }
+}
