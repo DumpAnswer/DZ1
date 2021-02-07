ba3c67852a9033fd3f22176694743c70208f6e22
DumpAnswer <kaleksers93@gmail.com>
2021-02-07 15:23:57 +0300

    Выполнил 1-ое задание

---
+++ Lesson 6/DZ6 Z1/ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿ using System;
+using System.Diagnostics;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process notepad = Process.Start("notepad");
+            Process[] local = Process.GetProcessesByName("notepad");
+            int i = local.Length;
+            
+            foreach(Process process in Process.GetProcesses())
+            {
+                Console.WriteLine($" ID : {process.Id} Name: {process.ProcessName}");
+
+            }
+
+            while(i > 0)
+            {
+                Console.WriteLine("Введите id процеса для его завершения :");
+                string id = Console.ReadLine();
+                if (string.IsNullOrEmpty(id))
+                    break;
+
+                try
+                {
+                    using (Process chosen = Process.GetProcessById(int.Parse(id)))
+                    {
+                        
+                        if (chosen.Id ==  int.Parse(id))
+                        {
+                            chosen.Kill();
+                            chosen.WaitForExit();
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Неверное значение");
+                    continue;
+                }
+                
+            }
+        }
+    }
+}
